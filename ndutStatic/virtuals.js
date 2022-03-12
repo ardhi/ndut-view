84d1a7d176327f3921f73de40650f2c36f28c745
Ardhi Lukianto <ardhi@lukianto.com>
2022-03-12 23:11:45 +0700

    Add holderjs

--- ndutStatic/virtuals.js
+++ ndutStatic/virtuals.js
@@ -3,11 +3,13 @@ module.exports = async function (scope) {
   const { pathResolve, getModuleDirDeep } = aneka
   const bsDir = _.dropRight(pathResolve(require.resolve('bootstrap')).split('/'), 3).join('/') + '/dist'
   const bsIconDir = _.dropRight(pathResolve(require.resolve('bootstrap-icons/package.json')).split('/'), 1).join('/')
+  const holderDir = _.dropRight(pathResolve(require.resolve('holderjs/package.json')).split('/'), 1).join('/')
   const lodashDir = getModuleDirDeep('lodash')
   const luxonDir = getModuleDirDeep('luxon') + '/build/global'
   return [
     { root: bsDir, prefix: 'bootstrap' },
     { root: bsIconDir, prefix: 'bootstrap-icons' },
+    { root: holderDir, prefix: 'holder' },
     { root: lodashDir, prefix: 'lodash' },
     { root: luxonDir, prefix: 'luxon' }
   ]
